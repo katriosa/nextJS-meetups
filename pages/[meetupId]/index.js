ac431f768a259c3ca227e376d8507a05218a95ff
katriosa <ee.chistyakova@gmail.com>
2023-03-09 17:09:53 +0700

    show details meetup

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Kastellet_citadel_on_Kastellholmen_Stockholm_2016_02.jpg/1024px-Kastellet_citadel_on_Kastellholmen_Stockholm_2016_02.jpg"
+      title="A first Meetup in Stockholm"
+      address="Some street, 5"
+      description="The meetup description"
+    />
+  );
+}
+export default MeetupDetails;
