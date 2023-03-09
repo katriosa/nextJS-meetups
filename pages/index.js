31a0cc3d6514ef254e039fcbcd8efa1fe7cdb87a
katriosa <ee.chistyakova@gmail.com>
2023-03-09 17:31:56 +0700

    pre-rendering by getStaticProps()

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,16 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
+export async function getStaticProps() {
+  //fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
+
 export default HomePage;
