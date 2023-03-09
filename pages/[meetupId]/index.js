93a6e0278695cf95b9f6f48f6695bab2018d8017
katriosa <ee.chistyakova@gmail.com>
2023-03-09 18:39:53 +0700

    getStaticProps for dynamic pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,4 +10,41 @@ function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false, // all paths
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for single meetup
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Kastellet_citadel_on_Kastellholmen_Stockholm_2016_02.jpg/1024px-Kastellet_citadel_on_Kastellholmen_Stockholm_2016_02.jpg",
+        title: "A first Meetup in Stockholm",
+        address: "Some street, 5",
+        description: "The meetup description",
+      },
+    },
+  };
+}
 export default MeetupDetails;
