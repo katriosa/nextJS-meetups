d738813040f60a37920e1a9b21727a87648afadd
katriosa <ee.chistyakova@gmail.com>
2023-03-13 11:56:24 +0700

    get data from database

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
 const DUMMY_MEETUPS = [
   {
@@ -37,9 +38,25 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   //fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb+srv://thekatrin:1029384756am@cluster0.y61lx2d.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1,
   };
